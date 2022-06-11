767f8c9c4c7945b14c91a587dc2b7e7e1685515d
Vedant Sheoran <vedantsheoran01@gmail.com>
2022-06-11 15:03:19 +0530

    Update Hackerrank query.sql

--- Hackerrank query.sql
+++ Hackerrank query.sql
@@ -1,1 +1,14 @@
+SELECT * 
+FROM CITY
+WHERE population>100000 AND countrycode='USA';
 
+//EXP : it will print the city where population > 100000 and USa
+
+--Select All 
+SELECT *
+FROM city;
+
+--Select By ID 
+SELECT *
+FROM city
+WHERE id=1661;
