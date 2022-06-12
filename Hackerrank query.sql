1934cf6515ac8caf0ed3a40cab8b4b077745f821
Vedant Sheoran <vedantsheoran01@gmail.com>
2022-06-12 20:35:10 +0530

    Update Hackerrank query.sql

--- Hackerrank query.sql
+++ Hackerrank query.sql
@@ -12,3 +12,26 @@ FROM city;
 SELECT *
 FROM city
 WHERE id=1661;
+
+--Select All 
+SELECT *
+FROM city;
+
+--Select By ID 
+SELECT *
+FROM city
+WHERE id=1661;
+
+--Japanese Cities' Attributes 
+SELECT *
+FROM city
+WHERE countrycode='JPN';
+
+--Japanese Cities' Names 
+SELECT name
+FROM city
+WHERE countrycode='JPN';
+
+--Weather Observation Station 1 
+SELECT city,state
+FROM station;
